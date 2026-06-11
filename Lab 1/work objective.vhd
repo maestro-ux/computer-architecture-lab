ffe2df31d76cb97abdbdd0c9b0acd650926fe9c8
saugat shrestha <217181962+maestro-ux@users.noreply.github.com>
2026-06-11 21:15:49 +0545

    Create work objective.vhd

---
+++ Lab 1/work objective.vhd
@@ -0,0 +1,7 @@
+v 4
+file . "buffer_tb.vhd" "cdb5dc77d0584dd3bf9a3690bc6a4e4a65d5b1f3" "20260505150011.027":
+  entity buffer_tb at 1( 0) + 0 on 13;
+  architecture simulation of buffer_tb at 7( 93) + 0 on 14;
+file . "buffer.vhd" "b3e9e010160a30ba792e56932c430fe6c0caac14" "20260505150011.008":
+  entity my_buffer at 2( 59) + 0 on 11;
+  architecture dataflow of my_buffer at 14( 316) + 0 on 12;
