6e310c1bfb2c4c8c838cee53f74ffd892c5bacc7
saugat shrestha <217181962+maestro-ux@users.noreply.github.com>
2026-07-04 07:01:47 +0545

    Update and rename Bin to Gray.vhd to Bin-Gray.vhd

--- LAB 6/Bin-Gray.vhd
+++ LAB 6/Bin-Gray.vhd
@@ -10,7 +10,7 @@ end entity BIN_TO_GRAY;
 
 architecture Dataflow of BIN_TO_GRAY is
 begin
-    G(3) <= B(3);                -- MSB stays the same
+    G(3) <= B(3);                
     G(2) <= B(3) xor B(2);
     G(1) <= B(2) xor B(1);
     G(0) <= B(1) xor B(0);
